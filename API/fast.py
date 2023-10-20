8aa909aef60322fa402ee1d631c514e4fb310989
Connor Power <conye@Connors-Mac-mini-2.local>
2023-10-19 20:29:54 -0400

    Updated fast.py with new OpenAI compatible endpoint

--- API/fast.py
+++ API/fast.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import time
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
@@ -10,12 +12,21 @@ from examples.advanced.sdxl.sdxl import SDXL
 from pydantic import BaseModel
 from io import BytesIO
 from time import sleep
-import os
-# Importing the SDXL model
+from typing import List
 from examples.advanced.sdxl.sdxl import SDXL
 
 class ImageRequest(BaseModel):
     prompt: str
+
+class OpenAIRequest(BaseModel):
+    prompt: str
+    n: int
+    size: str
+
+class OpenAIResponse(BaseModel):
+    created: int
+    data: List[dict]
+
 
 app = FastAPI()
 
@@ -32,13 +43,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Initialize the SDXL model
 model = SDXL()
 
+#Generate Image from GPUtopia
 @app.post("/generate-custom-image")
 def generate_custom_image(request: ImageRequest):
     # Use the SDXL model to generate an image based on the provided prompt
     response = model.run(prompt=request.prompt)
-    
     # Return the generated image
     return response
+
+
+#Generate image from GPUtopia API
+#Request & Response in OpenAI API format
+@app.post("/openai-compat", response_model=OpenAIResponse)
+def openai_compat_endpoint(request: OpenAIRequest):
+
+    # Note: May need to adjust this part based on how SDXL model handles multiple image generation (n parameter)
+    images = [model.run(prompt=request.prompt) for _ in range(request.n)]
+    
+    # Construct the response
+    response_data = [{"url": image_url} for image_url in images]
+
+    response = {
+        "created": int(time.time()),
+        "data": response_data
+    }
+    
+    return response
+
